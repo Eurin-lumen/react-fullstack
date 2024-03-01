e44678b5232064715976f31ed5a9405504e7f598
Eurin HASH <eurindelagoldcoast@gmail.com>
2024-03-01 11:01:00 +0100

    Les dossier de base

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {createBrowserRouter} from  'react-router-dom'
+
+// creation du routeur  : de l'objet browser router 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    
+  }
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
