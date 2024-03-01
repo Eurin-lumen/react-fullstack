2b65148c08bd7b9829c8b22e683e5b90a528b5f1
Eurin HASH <eurindelagoldcoast@gmail.com>
2024-03-01 11:09:18 +0100

    importation des routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {createBrowserRouter} from  'react-router-dom'
+import Dashbord from './pages/dahsbord/Dashbord.jsx'
+import Connexion from './pages/connexion/Connexion.jsx'
+import Inscription from './pages/inscription/Inscription.jsx'
 
 // creation du routeur  : de l'objet browser router 
 const router = createBrowserRouter([
   {
     path: "/",
-    
+    element: <Dashbord />
+  },
+  {
+    path: "/connexion",
+    element: <Connexion />
+  },
+  {
+    path: "/inscription",
+    element: <Inscription />
   }
 ])
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
