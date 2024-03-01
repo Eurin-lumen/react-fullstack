72577068844d62a1da5106ed999e5a3abe1573b0
Eurin HASH <eurindelagoldcoast@gmail.com>
2024-03-01 12:20:26 +0100

    Faire fonctionner les route en mode teste

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {createBrowserRouter} from  'react-router-dom'
-import Dashbord from './pages/dahsbord/Dashbord.jsx'
-import Connexion from './pages/connexion/Connexion.jsx'
-import Inscription from './pages/inscription/Inscription.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from "./App.jsx";
+import { createBrowserRouter, BrowserRouter, RouterProvider } from 'react-router-dom';
+import Dashbord from './pages/dashbord/Dashbord.jsx';
+import Connexion from './pages/connexion/Connexion.jsx';
+import Inscription from './pages/inscription/Inscription';
 
-// creation du routeur  : de l'objet browser router 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashbord />
-  },
-  {
-    path: "/connexion",
-    element: <Connexion />
-  },
-  {
-    path: "/inscription",
-    element: <Inscription />
-  }
-])
 
+// Creation de l'objet Browser router 
+
+const router  = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element:<Dashbord/>,
+    },
+    {
+      path: "/connexion",
+      element: <Connexion/>,
+    },
+    
+    {
+      path: "/inscription",
+      element: <Inscription/>
+    }
+  ]
+)
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    
+    <RouterProvider router={router}>
+
+    </RouterProvider>
+  </React.StrictMode>
+);
--- src/pages/inscription/Inscription.jsx
+++ src/pages/inscription/Inscription.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Inscription() {
   return (
     <div>
-      
+      Inscription
     </div>
   )
 }
