e33395e78a4f49322322db88c430aa67141659f0
Eurin HASH <eurindelagoldcoast@gmail.com>
2024-03-02 22:54:23 +0100

    ok

--- src/pages/inscription/Inscription.jsx
+++ src/pages/inscription/Inscription.jsx
@@ -13,10 +13,12 @@ export default function InscriptionPage() {
         }
       } >
         <Typography variant="h4" sx={{textAlign:"center"}}>Inscription</Typography> <br />
-        <form action="#" method="post">
+        <form action="" method="post">
           <Stack flexDirection={"column"} gap={2}>
 
             <TextField id="outlined-basic" label="Veuillez saisir votre nom" variant="outlined" />
+
+            <TextField id="outlined-basic" label="Veuillez saisir votre email" type="email"variant="outlined" />
 
             <TextField id="outlined-basic" label="Veuillez saisir un mot de passe"  type="password" variant="outlined" />
 
